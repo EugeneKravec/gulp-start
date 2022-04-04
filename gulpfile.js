d28af610aab9012a7f8c8b48428a99df2d63a327
Eugene Kravec <krava255@gmail.com>
2022-04-05 00:36:58 +0300

    install dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const scss = require('gulp-sass')(require('sass'));
-// const concat = require('gulp-concat');
+const concat = require('gulp-concat');
+const browseSync = require('browser-sync').create();
 
+function browsersync() {
+    browseSync.init({
+        server: {
+            baseDir: 'app/'
+        }
+    })
+};
+
+// Minimisation the scss and reload page
 function styles() {
     return src('app/scss/style.scss')
         .pipe(scss({ outputStyle: 'compressed' })) // { outputStyle: 'compressed' }
-        // .pipe(concat('style.min.css'))
-        .pipe(dest('app/css'));
+        .pipe(concat('style.min.css'))
+        .pipe(dest('app/css'))
+        .pipe(browseSync.stream())
 };
 
+function watching() {
+    watch(['app/scss/**/*.scss'], styles)
+    watch(['app/*.html']).on('change', browseSync.reload)
+}
+
 exports.styles = styles;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.default = parallel(browsersync, watching)
