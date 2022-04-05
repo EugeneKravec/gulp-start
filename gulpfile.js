d7bc2e716f43fb4b102c3da5dda55d0948f37056
Eugene Kravec <krava255@gmail.com>
2022-04-06 00:14:02 +0300

    Ended up with GULP
    to build

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-const { src, dest, watch, parallel } = require('gulp');
+const { src, dest, watch, parallel, series } = require('gulp');
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
 const browseSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
 
 function browsersync() {
     browseSync.init({
@@ -11,22 +15,72 @@ function browsersync() {
     })
 };
 
+function images() {
+    return src('app/images/**/*')
+        .pipe(imagemin([
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.mozjpeg({ quality: 75, progressive: true }),
+            imagemin.optipng({ optimizationLevel: 5 }),
+            imagemin.svgo({
+                plugins: [
+                    { removeViewBox: true },
+                    { cleanupIDs: false }
+                ]
+            })
+        ]))
+        .pipe(dest('dist/images'))
+}
+
+function scripts() {
+    return src([
+        'node_modules/jquery/dist/jquery.js',
+        'app/js/main.js'
+    ])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browseSync.stream())
+};
+
+function cleanDist() {
+    return del('dist')
+}
+
 // Minimisation the scss and reload page
 function styles() {
     return src('app/scss/style.scss')
         .pipe(scss({ outputStyle: 'compressed' })) // { outputStyle: 'compressed' }
         .pipe(concat('style.min.css'))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 10 version'],
+            grid: true
+        }))
         .pipe(dest('app/css'))
         .pipe(browseSync.stream())
 };
 
+function build() {
+    return src([
+        'app/css/style.min.css',
+        'app/fonts/**/*',
+        'app/js/main.min.js',
+        'app/*.html',
+    ], { base: 'app' })
+        .pipe(dest('dist'))
+}
+
 function watching() {
     watch(['app/scss/**/*.scss'], styles)
+    watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts)
     watch(['app/*.html']).on('change', browseSync.reload)
 }
 
 exports.styles = styles;
 exports.watching = watching;
 exports.browsersync = browsersync;
+exports.scripts = scripts;
+exports.images = images;
+exports.cleanDist = cleanDist;
 
-exports.default = parallel(browsersync, watching)
+exports.build = series(cleanDist, images, build);
+exports.default = parallel(styles, scripts, browsersync, watching)
